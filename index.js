4c7f316ce01583b4f547414980fd1329180642ea
Joseph Szpigiel <josephszpigiel@gmail.com>
2023-09-11 12:08:14 -0400

    Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location){
+    if (location >= 42){
+        return location - 42;
+    } else{
+        return 42 - location;
+    }
+}
+function distanceFromHqInFeet(location){
+    let blocks = distanceFromHqInBlocks(location);
+    return blocks * 264;
+}
+function distanceTravelledInFeet(start,stop){
+    let distance = (start >= stop ? start - stop : stop - start);
+    return distance * 264;
+}
+function calculatesFarePrice(start, stop){
+    let feetTraveled = distanceTravelledInFeet(start,stop);
+    switch (true){
+        case feetTraveled <= 400:
+            return 0;
+        case feetTraveled >= 2500:
+            return `cannot travel that far`;
+        case feetTraveled > 2000:
+            return 25;
+        case feetTraveled > 400:
+            return .02 * (feetTraveled - 400);
+    }
+}
